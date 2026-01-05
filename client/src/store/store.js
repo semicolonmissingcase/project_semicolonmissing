75e562b9ca80a0e03c413e10e93dbbb80d49cc65
jeongyeonjae0 <jeongyeonjae0@gmail.com>
2026-01-05 19:58:02 +0900

    260105 [client] ChatRoom 수정, 관리자 로그인, adminAuthReducer 추가, adminAuthSlice, adminAuthThunk 추가

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authRouter from './slices/authSlice.js';
 import cleanersReducer from './slices/cleanersSlice.js';
+import adminAuthReducer from './slices/adminAuthSlice.js';
 
 export default configureStore({
   reducer: {
     auth: authRouter,
+    adminAuth: adminAuthReducer,
     cleaners: cleanersReducer,
   }
 });
