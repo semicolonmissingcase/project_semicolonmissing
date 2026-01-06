457ca371ef7f3c657ccdaab77ab442ae777b74ca
jeongyeonjae0 <jeongyeonjae0@gmail.com>
2026-01-06 13:10:21 +0900

    260106 [server] auth.middleware.js로 통합( auth.admin.middleware.js , auth.user.middleware.js 삭제)

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -8,14 +8,14 @@ import express from 'express';
 import adminLoginValidator from '../app/middlewares/validations/validatiors/auth/adminLogin.validator.js';
 import validationHandler from '../app/middlewares/validations/validationHandler.js';
 import { adminController } from '../app/controllers/auth/admin.controller.js';
-import authAdminMiddleware from '../app/middlewares/auth/auth.admin.middleware.js';
+import authMiddleware from '../app/middlewares/auth/auth.middleware.js';
 import adminDashboardController from '../app/controllers/admin/admin.dashboard.controller.js';
  
 const adminRouter = express.Router();
 
 adminRouter.post('/login', adminLoginValidator, validationHandler, adminController.adminLogin);
-adminRouter.post('/logout', authAdminMiddleware, validationHandler, adminController.adminLogout);
+adminRouter.post('/logout', authMiddleware, validationHandler, adminController.adminLogout);
 adminRouter.post('/reissue', adminController.reissue);
-adminRouter.get('/monitoring', authAdminMiddleware, adminDashboardController.getMonitoringData);
+adminRouter.get('/monitoring', authMiddleware, adminDashboardController.getMonitoringData);
 
 export default adminRouter;
